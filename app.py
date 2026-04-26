b4193bdf228df744a9a8ea6d3484bdec7ac334d7
nasermena <nasermena@gmail.com>
2026-04-26 11:00:07 +0300

    Import add_numbers in app.py and use it to print sum result

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+from helper import add_numbers
+
 def greet(name):
-    return f"أهلاً {name}! هذا أول مشروع Python مع Git 🎉"
+    return f"Hello {name}! This is my first Python project with Git 🎉"
 
 print(greet("Naser"))
+
+result = add_numbers(10, 20)
+print(f"The sum of 10 and 20 is: {result}")
--- helper.py
+++ helper.py
@@ -1,7 +1,5 @@
 def add_numbers(a, b):
     return a + b
 
-print(add_numbers(3, 5))
-
 def multiply_numbers(a, b):
     return a * b
